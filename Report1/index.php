2820d9b9e370635703e7f08c3193fdcd950d8216
cosky4sure <cosky4sure@gmail.com>
2016-10-23 08:27:02 +0300

    MyTestProject

---
+++ Report1/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+ <link rel="stylesheet" type="text/css"  href="color.css"/>
+<title>MIS</title>
+
+		<style>
+		.button {
+	  display: inline-block;
+	  padding: 8px 15px;
+	  font-size: 10px;
+	  cursor: pointer;
+	  text-align: center;	
+	  text-decoration: none;
+	  outline: none;
+	  color: #fff;
+	  background-color:green;
+	  border: none;
+	  border-radius: 8px;
+	  box-shadow: 0 5px #999;
+	}
+
+	.button:hover {background-color: blue}
+
+	.button:active {
+	  background-color: #3e8e41;
+	  box-shadow: 0 5px #666;
+	  transform: translateX(4px);
+	}
+
+
+		</style>
+</head>
+<body style="
+	background-color:#E6E6FA;
+	background-repeat: repeat;
+    border-width: 50px;    
+	border-style: solid;    
+	border-color: purple;
+	margin:auto;
+	margin-top:2cm;
+	width:60%;   
+    padding:50px;
+    left: 120px;   
+	">
+
+		
+<form class="contact_form  "action="verify_index.php" method="POST" name="contact_form" style="color:blue;">
+
+
+	  <ul style="list-style-type:none;">
+
+			<li>
+			
+			 <label for "name"><h2>Teachers Login </h2></label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp||&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<label><a href="admin.php"><h2 style="color:blue">Admin Login </h2></a></label>
+	
+			
+			</li>
+			<li>
+			<hr class="horizontal"/>
+			</li></br>
+			<li>
+			
+		   <label for "name"> Username:</label> 
+		   <input type="text" name="admin" placeholder="chemistry or physics or english or mathematics" required></br></br>
+		   
+		   </li>
+		   <li>
+		     <label for "name"> Password:</label> 
+			<input type="password"  name="password" placeholder="password" required></br></br>
+		   </li>
+		   <li>
+		     <label for "name"> </label> 
+			 <button type="submit" class="button">Login</button>
+
+		   </li>
+		   <li>
+			</br><hr class="horizontal2"/>
+			</li></br>
+	   </ul>
+ 
+</body>
+</html>
